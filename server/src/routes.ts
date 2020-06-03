f4e2aaa312e631ea1b328fb28f24c6ac7e2d3f4b
Sabrina Veras <sabrina_veras@live.com>
2020-06-03 00:08:12 -0300

    install database, controller, routes, and more

---
+++ server/src/routes.ts
@@ -0,0 +1,21 @@
+import express, {json} from "express";
+
+import PointsController from "./controllers/pointsController";
+import ItemsController from "./controllers/itemsController";
+
+const routes = express.Router();
+
+// index, show, create, update, delete
+
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get("/items", itemsController.index);
+
+
+
+routes.post("/points", pointsController.create);
+routes.get("/points/", pointsController.index);
+routes.get("/points/:id", pointsController.show);
+
+export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,17 @@
 import express from "express";
+import cors from "cors";
+import path from "path";
+
+import routes from "./routes";
 
 const app = express();
 
-app.get("/users", (request, response) =>{
-    console.log("listing users");
+app.use(cors());
 
-    response.json(["Sabrina", "Veras", "Higor"])
-});
+app.use(express.json());
+
+app.use(routes);
+
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333);
